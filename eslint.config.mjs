d3cb0d437710239f97d9b933db04d7d4f969b39c
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-29 17:33:53 -0300

    fix: adjust eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
 import config from '@whoisfreire/eslint-config'
+import { globalIgnores } from 'eslint/config'
 
-export default config
+export default [
+  ...config,
+  globalIgnores([
+    'data',
+    'node_modules',
+  ])
+]
